e937b78c56342433d78edb83a2e28ef345b98ec9
matteu61 <36701863+matteu61@users.noreply.github.com>
2025-11-19 13:38:27 +0200

    try again wit images
    trying again

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "600964881"
+    serviceWorkerVersion: "3238187359"
   }
 });
